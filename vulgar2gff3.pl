3dc51da8b53767f90e7a74f9db36c1dde1cdf999
Ari Löytynoja <ari.loytynoja@gmail.com>
2019-04-05 15:24:25 +0300

    fixed license

--- vulgar2gff3.pl
+++ vulgar2gff3.pl
@@ -1,24 +1,24 @@
 #!/usr/bin/perl
 
-/***************************************************************************
- *   Copyright (C) 2018-2019 by Ari Loytynoja                              *
- *   ari.loytynoja@gmail.com                                               *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+#/***************************************************************************
+# *   Copyright (C) 2018-2019 by Ari Loytynoja                              *
+# *   ari.loytynoja@gmail.com                                               *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 2 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program; if not, write to the                         *
+# *   Free Software Foundation, Inc.,                                       *
+# *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# ***************************************************************************/
 
 #
 # exonerate -m p2g -t region.nuc -q prot.pep --showalignment F --showtargetgff F --useaatla F > exonerate.vul
